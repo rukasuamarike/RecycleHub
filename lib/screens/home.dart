3c1edbefe814d277223942d7a6cc6e0a294ef826
Lucas Amlicke <lukeamlicke@gmail.com>
2023-02-25 15:01:10 -0800

    home [added popups]

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
-
+import 'dart:core';
+import 'dart:html' as html;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
 import 'package:recyclehub/widgets/userdata.dart';
+import '../widgets/crv.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -20,6 +24,23 @@ class _HomeState extends State<Home> {
     User? user = Provider.of<User?>(context);
     UserData? userData = Provider.of<UserData?>(context);
 
+    void mapuri() async {
+      if (userData!.defaultZip == 0) {
+        showDialog(
+            context: context,
+            builder: (BuildContext) => _SetZipDialog(context, userData));
+      } else {
+        await FirebaseFirestore.instance
+            .collection("api")
+            .where("PostalCode", isEqualTo: userData.defaultZip)
+            .get()
+            .then((snap) => snap.docs.map((e) => CRV.fromFirestore(e)))
+            .then((crvs) => showDialog(
+                context: context,
+                builder: (BuildContext) => _CRVDialog(context, crvs)));
+      }
+    }
+
     return Scaffold(
         body: Center(
       child: Column(
@@ -33,7 +54,9 @@ class _HomeState extends State<Home> {
           InkWell(
             child: const Text("My Recycling Center"),
             //on tap launch a google maps link
-            onTap: () {},
+            onTap: () async {
+              mapuri();
+            },
           ),
           SizedBox(height: MediaQuery.of(context).size.width / 32),
 
@@ -47,4 +70,86 @@ class _HomeState extends State<Home> {
       ),
     ));
   }
+}
+
+Widget _SetZipDialog(BuildContext context, UserData uData) {
+  final zipController = TextEditingController(text: uData.name);
+  return AlertDialog(
+    title: Text("Set zipcode"),
+    content: SafeArea(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextField(
+            controller: zipController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: "Enter your zipcode"),
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+          ),
+          TextButton(
+            onPressed: () async {
+              UserData temp = uData;
+              if (zipController.text.isNotEmpty &&
+                  zipController.text.length < 6) {
+                temp.defaultZip = int.parse(zipController.text);
+              }
+              await FirebaseFirestore.instance
+                  .collection("Users")
+                  .doc(uData.uid)
+                  .update(uData.toJson())
+                  .then((k) => Navigator.of(context).pop());
+            },
+            child: Text("Set"),
+          ),
+        ],
+      ),
+    ),
+    actions: [
+      TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("Cancel")),
+    ],
+  );
+}
+
+Widget _CRVDialog(BuildContext context, Iterable<CRV> datas) {
+  List<CRV> lv = datas.toList();
+  return AlertDialog(
+    title: Text("Select shop"),
+    content: SafeArea(
+      child: ListView.builder(
+          itemCount: lv.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(lv[index].FacilityName),
+              subtitle: TextButton(
+                onPressed: () async {
+                  String query = Uri.encodeComponent(
+                      '${lv[index].StreetAddress}, ${lv[index].City},CA ');
+                  html.window.open(
+                      'https://www.google.com/maps/search/?api=1&query=centurylink+field?api=1&query=${query}',
+                      "bob");
+                },
+                child: Text('${lv[index].StreetAddress}, ${lv[index].City}'),
+
+                ///
+                /// TODO: upload resume button
+                ///
+              ),
+            );
+          }),
+    ),
+    actions: [
+      TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("Cancel")),
+    ],
+  );
 }
