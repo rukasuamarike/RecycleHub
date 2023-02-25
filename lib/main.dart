574f7f574c5796a68a11321444755ea0e49f4e83
RaphaLK <raphaelkusuma5@gmail.com>
2023-02-24 23:46:46 -0800

    Firebase Setup in Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
 }
 
 class MyApp extends StatelessWidget {
