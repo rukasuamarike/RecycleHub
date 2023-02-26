8510d99a9eaa191db5c1167e18115db1db7cb572
nathany9 <76071239+nathany9@users.noreply.github.com>
2023-02-26 00:01:14 -0800

    leaderboard

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../widgets/userdata.dart';
 
 class Leaderboard extends StatefulWidget {
   const Leaderboard({super.key});
@@ -11,6 +15,51 @@ class Leaderboard extends StatefulWidget {
 class _LeaderboardState extends State<Leaderboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Future<List<UserData>> Leaders = FirebaseFirestore.instance
+        .collection("Users")
+        .orderBy("totalCans", descending: true)
+        .limit(50)
+        .get()
+        .then((snap) =>
+            snap.docs.map((doc) => UserData.fromFirestore(doc)).toList());
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SafeArea(
+                  child: FutureBuilder(
+                      future: FirebaseFirestore.instance
+                          .collection("Users")
+                          .orderBy("totalCans", descending: true)
+                          .limit(50)
+                          .get(),
+                      builder: (context, AsyncSnapshot snapshot) {
+                        if (!snapshot.hasData) {
+                          return Center(child: CircularProgressIndicator());
+                        } else {
+                          List<UserData> usrlist = snapshot.data
+                              .map((doc) => UserData.fromFirestore(doc))
+                              .toList();
+                          return Container(
+                              child: ListView.builder(
+                                  itemCount: usrlist.length,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Text(usrlist[index].name);
+                                  }));
+                        }
+                      })),
+              const Text(
+                "Leaderboard",
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
